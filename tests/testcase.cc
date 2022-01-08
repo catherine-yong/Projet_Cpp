f0c3385e92406f3bfbb0a55f5e30206d9da9e9c9
Catherine Yong <47810291+catherine-yong@users.noreply.github.com>
2022-01-08 23:15:09 +0100

    Cathy v_3

--- tests/testcase.cc
+++ tests/testcase.cc
@@ -12,20 +12,71 @@
 #include "soleil.hh"
 #include "type.hh""
 
-TEST_CASE("1: Creation Soleil")
+TEST_CASE("1 : Constructeurs")
 {
-  Soleil a , b(a) , c("soleil_c",50,10,300);
+  SECTION("Soleil")
+  {
+    Soleil a , b(a) , c("soleil_c",50,10,300);
+    
+    REQUIRE(a.get_nom() == "Sun");
+    REQUIRE(b == a);
+    REQUIRE(c.get_nom() == "soleil_c");
+    REQUIRE(c.get_masse() == 50);
+    REQUIRE(c.get_volume() == 10);
+    REQUIRE(c.get_prix() == 300);
+  }
+
+  SECTION("2 : Planetes")
+  {
+    Planete p1 , p2(p1) , p3("Planete_TERRE",33,1000,26,500,3);
   
-  REQUIRE(a.get_nom() == "Sun");
-  REQUIRE(b.get_nom() == a.get_nom());
-  REQUIRE(b.get_masse() == a.get_masse());
-  REQUIRE(b.get_volume() == a.get_volume());
-  REQUIRE(b.get_prix() == a.get_prix());
-  REQUIRE(b.get_nom() == "soleil_c");
-  REQUIRE(b.get_masse() == 50);
-  REQUIRE(b.get_volume() == 10);
-  REQUIRE(b.get_prix() == 300);
-  
+    REQUIRE(p1.get_nom() == "planet_1");
+    REQUIRE(p1.get_nb_lunes() == 2);
+    REQUIRE(p2 == p1);
+    REQUIRE(p3.get_nom() == "Planete_TERRE");
+    REQUIRE(p3.get_volume() == 33);
+    REQUIRE(p3.get_distance() == 1000);
+    REQUIRE(p3.get_masse() == 26);
+    REQUIRE(p3.get_prix() == 500);
+    REQUIRE(p3.get_nb_lunes() == 3);
+
+  }
+
+  SECTION("3 : Lunes")
+  {
+    Lune l1, l2(l1), l3("moony",5,50,90);
+
+    REQUIRE(l1.get_nom() == "moon_0");
+    REQUIRE(l1.get_prix() == 30);
+    REQUIRE(l2 == l1);
+    REQUIRE(l3.get_nom() == "moony");
+    REQUIRE(l3.get_weight() == 5);
+    REQUIRE(l3.get_distance() == 50);
+    REQUIRE(l3.get_prix() == 90);
+  }
+
+  SECTION("4 : Joueurs")
+  {
+    Joueur paul, julie(paul), marie("marie","%");
+
+    REQUIRE(paul.get_nom() == "joueur_0");
+    REQUIRE(julie.get_nom() == "joueur_1");
+    REQUIRE(marie.get_nom() == "marie");
+    REQUIRE(paul.get_budget() == 1000);
+    REQUIRE(julie.get_budget() == 1000);
+    REQUIRE(marie.get_budget() == 1000);
+
+  }
+
+  SECTION("5 : Fusee")
+  {
+    Fusee f1, f2(f1), f3("Rollie",2000,800);
+
+    REQUIRE(f1.get_nom() == "fusee_0");
+    REQUIRE(f2.get_nom() == "fusee_1");
+    REQUIRE(f1 == f2);
+    REQUIRE(f3.get_nom() == "Rollie");
+    REQUIRE(f3.get_annee() == 2000);
+    REQUIRE(f3.get_prix() == 800);
+  }
 }
-
-
